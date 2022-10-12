848d7e6006788eb190da206676977e6dacc378d3
prodromospapa <prodromospapa@gmail.com>
2022-10-12 15:08:33 +0300

    thesis

--- merge_samples.py
+++ merge_samples.py
@@ -6,12 +6,12 @@ samples_files_n=[filename for filename in os.listdir(f'{country}/tables_gisaid')
 
 dates = []
 for sample_n in samples_files_n:
-    dates += pd.read_pickle(f"{country}/tables_gisaid/samples_{sample_n}.pickle").columns.tolist()
+    dates += pd.read_pickle(f"{country}/tables_gisaid/{sample_n}").columns.tolist()
 
 dates = set(dates)
 samples = pd.DataFrame(0, index=['n_samples'], columns=dates)
 for sample_n in samples_files_n:
-    table = pd.read_pickle(f"{country}/tables_gisaid/samples_{sample_n}.pickle")
+    table = pd.read_pickle(f"{country}/tables_gisaid/{sample_n}")
     samples[table.columns.tolist()] = samples[table.columns.tolist()].add(table)
 
 samples.to_pickle(f"{country}/tables_gisaid/samples.pickle")
