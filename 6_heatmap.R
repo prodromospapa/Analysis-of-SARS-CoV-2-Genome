5373cee99f701611224fc4639e61902bddbfc253
prodromospapa <prodromospapa@gmail.com>
2022-10-14 12:07:01 +0300

    thesis

--- 6_heatmap.R
+++ 6_heatmap.R
@@ -5,7 +5,15 @@ country <- toString(read.table("country.txt"))[1]
 data <- read.table(paste(country,"/heatmap/heatmap.csv",sep=""),sep=",",header=TRUE,row.names=1,quote = "", stringsAsFactors = FALSE)
 colnames(data) <- as.Date(gsub("^X", "", colnames(data)),tryFormats = c("%m_%d_%Y"))
 
-m <- as.matrix(data)
 pdf(file=paste(country,".pdf",sep=""))
-heatmap.2(m,dendogram='none',scale='row',Rowv=NULL,Colv=NULL)
+heatmap.2(as.matrix(data),
+    scale="row", 
+    key=T, 
+    keysize=1,
+    density.info="none", 
+    trace="none",
+    dendogram='none',
+    Rowv=NULL,
+    Colv=NULL)
+dev.off()
 dev.off()
