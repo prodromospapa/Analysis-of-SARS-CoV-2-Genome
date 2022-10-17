cbdb1aaaf832f5af10cf51cc3e233e77a8ab6ba3
prodromospapa <prodromospapa@gmail.com>
2022-10-17 22:44:09 +0300

    thesis

--- fisher.py
+++ fisher.py
@@ -5,7 +5,9 @@ import numpy as np
 import datetime
 from statsmodels.stats.multitest import fdrcorrection
 import sys
+import warnings
 
+warnings.filterwarnings('ignore')#mutes pandas fragmentation warning
 #file format month_day_year
 
 country=open("country.txt").readline().strip()
@@ -15,10 +17,9 @@ sra_list = open(sra_txt)
 vcf_list = os.popen(f"find {country}/vcf_ncbi/*/*.vcf").read().split("\n")[:-1]
 vcf_list = [i.split("/")[-1].strip('.vcf') for i in vcf_list ]
 
-for sample in sra_list:
-    counter = os.popen(f"less {country}/p_tables/*/*.csv | wc -l").read()
+for sample in sra_list.read().split("\n")[:-1]:
     if sample in vcf_list:
-        date = os.popen(f"find {country}/vcf_ncbi/*/{sample.strip()}.vcf").read().split("/")[1]
+        date = os.popen(f"find {country}/vcf_ncbi/*/{sample.strip()}.vcf").read().split("/")[2]
         os.system(f"mkdir -p {country}/p_tables/{date}")
         if f"{sample.strip()}.csv" not in os.listdir(f"{country}/p_tables/{date}"):
             #window list
