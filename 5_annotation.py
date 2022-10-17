04bce64802a2817b553d8f8e03f4aca8ea8077a0
prodromospapa <prodromospapa@gmail.com>
2022-10-18 00:26:34 +0300

    thesis

--- 5_annotation.py
+++ 5_annotation.py
@@ -23,7 +23,7 @@ for gene in genes[1:]:
 
 country=open("country.txt").readline().strip()
 
-dates = os.listdir("p_tables")
+dates = os.listdir(f"{country}/p_tables")
 dates = [datetime.datetime.strptime(ts, "%m_%d_%Y") for ts in dates]
 dates.sort()
 dates = [datetime.datetime.strftime(ts, "%m_%d_%Y") for ts in dates]
@@ -34,18 +34,19 @@ total = int(os.popen(f"find {country}/p_tables/*/*.csv | wc -l").read())
 count = 0
 os.system(f"mkdir -p {country}/heatmap")
 for day in dates:
-    vcfs = os.listdir(f"{country}/p_tables/{day}")
-    for vcf in vcfs:
+    csvs = os.listdir(f"{country}/p_tables/{day}")
+    for csv in csvs:
       count +=1
-      table = pd.read_csv(f"{country}/p_tables/{day}/{vcf}",index_col=0)
+      table = pd.read_csv(f"{country}/p_tables/{day}/{csv}",index_col=0)
       table = table[table<=0.05].dropna(axis = 0, how = 'all')
       for label in pos_dict:
         pos = pos_dict[label]
         number_of_diff = len(table.loc[pos[0]:pos[1]])
         final_table.at[label,day].append(number_of_diff)
-      print(f"{round(count/total,2)}%",end="\r")
+      print(f"{round((count/total)*100,2)}%",end="\r")
     
 
 final_table = final_table.applymap(lambda x : statistics.mean(x))
   
 final_table.to_csv(f"{country}/heatmap/heatmap.csv")
+print('all done')
