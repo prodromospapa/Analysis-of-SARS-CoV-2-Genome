04754241923fd6fcac15015348977ff410a14881
prodromospapa <prodromospapa@gmail.com>
2022-10-17 23:24:18 +0300

    thesis

--- 4_fisher.sh
+++ 4_fisher.sh
@@ -1,14 +1,16 @@
 country=$(less country.txt)
 total=$(find $country/vcf_ncbi/*/*.vcf | wc -l)
-counter="find ${country}/p_tables/*/*.csv | wc -l"
 command=""
 
 for text in $country/SraAccList/*
 do 
-    command="$command python3 fisher.py $text &"
+    number=$(echo $text | rev | cut -c1-6 | rev | cut -c1-2)
+    echo 0 > fisher_progress_$number.txt
+    command="$command python3 fisher.py $text $number &"
 done
 
-eval " $command bash progress.sh $total $counter "
+eval " $command bash progress.sh $total 'paste -d+ fisher_progress_*.txt| bc' "
 
 wait
-echo done
+rm fisher_progress_*.txt
+echo all done
--- fisher.py
+++ fisher.py
@@ -10,13 +10,17 @@ import sys
 
 country=open("country.txt").readline().strip()
 sra_txt = sys.argv[1]
+number = sys.argv[2]
 os.system(f"mkdir -p {country}/p_tables")
 sra_list = open(sra_txt)
 vcf_list = os.popen(f"find {country}/vcf_ncbi/*/*.vcf").read().split("\n")[:-1]
 vcf_list = [i.split("/")[-1].strip('.vcf') for i in vcf_list ]
+counter=0
 
 for sample in sra_list.read().split("\n")[:-1]:
     if sample in vcf_list:
+        counter+=1
+        os.system(f"echo {counter} > fisher_progress_{number}.txt")
         date = os.popen(f"find {country}/vcf_ncbi/*/{sample.strip()}.vcf").read().split("/")[2]
         os.system(f"mkdir -p {country}/p_tables/{date}")
         if f"{sample.strip()}.csv" not in os.listdir(f"{country}/p_tables/{date}"):
