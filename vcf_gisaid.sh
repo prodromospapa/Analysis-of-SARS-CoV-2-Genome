de6c6b228720ecd6add89fd087bbd3816f492a5b
prodromospapa <prodromospapa@gmail.com>
2022-10-19 17:40:39 +0300

    thesis

--- vcf_gisaid.sh
+++ vcf_gisaid.sh
@@ -2,7 +2,7 @@ country=$(less country.txt)
 refseq="EPI_ISL_402124"
 fastas=$@
 
-for fasta in $fasta:
+for fasta in $fastas:
 do
     counter=$(echo $fasta | sed "s/$country\/fasta\/$country//g" | sed 's/\.fasta//g')
     mafft --6merpair --thread -1 --keeplength --addfragments $fasta refseq/$refseq.fasta > $country/mafft/$country.$counter.mafft.fasta
