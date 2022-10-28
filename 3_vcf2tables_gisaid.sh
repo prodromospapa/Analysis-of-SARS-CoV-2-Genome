de8eddec7e610bfb30647aa6b1a1a5004d15f356
prodromospapa <prodromospapa@gmail.com>
2022-10-28 10:31:19 +0300

    thesis

--- 3_vcf2tables_gisaid.sh
+++ 3_vcf2tables_gisaid.sh
@@ -2,14 +2,14 @@ country=$(less country.txt)
 ram=$(grep MemTotal /proc/meminfo | awk '{print $2}')
 cpu_opt=$((ram/16000000))
 
-if [ $cpu_opt=0 ]
+if [ $cpu_opt == 0 ]
 then
     cpu_opt=1
 fi
-vcfs=($country/vcf_gisaid/*)
+vcfs=$country/vcf_gisaid/*
 
 items_per_thread=1
-while [ "${#vcfs}" -gt "$(($items_per_thread*$cpu_opt))" ]
+while [ $(echo $vcfs | wc -w) -gt $(($items_per_thread*$cpu_opt)) ]
 do
     items_per_thread=$((items_per_thread+1))
 done
@@ -19,19 +19,19 @@ samples=0
 total=0
 command=""
 
-for i in $country/vcf_gisaid/*
+for i in $vcfs
 do 
     total=$(($total + $(sed '4q;d' $i | grep -o hCoV-19 | wc -l) - 1 ))
 done
 
-while [ $counter -le "${#vcfs}" ]
+while [ $counter -le $(echo $vcfs | wc -w) ]
 do
     vcf2tables=""
     samples=$(($samples+1))
     for i in $(seq 1 $items_per_thread)
     do
-        vcf2tables="$vcf2tables ${vcfs[$counter]} "
         counter=$(($counter+1))
+        vcf2tables="$vcf2tables $(echo $vcfs | cut -d ' ' -f $counter) "
     done
     if [ ! "$vcf2tables" == "" ]
     then
