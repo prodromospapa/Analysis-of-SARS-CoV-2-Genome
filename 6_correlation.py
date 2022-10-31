ec129b2702a50022f31ae684f9482fa785e9b67e
prodromospapa <prodromospapa@gmail.com>
2022-11-01 01:24:59 +0200

    thesis

--- 6_correlation.py
+++ 6_correlation.py
@@ -1,7 +1,7 @@
 import pandas as pd
-import datetime
 import os
 import numpy as np
+from snapgene_reader import snapgene_file_to_dict, snapgene_file_to_seqrecord
 
 country=open("country.txt").readline().strip()
 os.system(f"mkdir -p {country}/correlation")
@@ -21,8 +21,28 @@ for day in dates:
     pickle = pickle.sum(axis=1).rename(day)
     table = pd.concat([table,pickle],axis=1)#enwnei oles tis hmeromhnies se mia
     counter+=1
-    print(f"{round(counter*100/total,2)}%",end="\r")
+    print(round(counter*100/total,2),end="\r")
 table = table.iloc[: , 1:].div(dates_table.iloc[0].tolist())#vgazei thn to_remove kai kanei normalizing
 
-table.to_csv(f"{country}/correlation/total_variant.csv")
-print('all done')
+dictionary = snapgene_file_to_dict('annotation.dna')
+CDS = [k for k in dictionary["features"] if k['type'] == "CDS"]
+mat_peptide = [k for k in dictionary["features"] if k['type'] == "mat_peptide"]
+genes = [k for k in dictionary["features"] if k['type'] == "gene"]
+
+pos_dict = {}
+
+for peptide in mat_peptide[:-1]:#petaei to teleftaio poy einai tou ORF1 mikro kommati
+  pos_dict[peptide['qualifiers']['product']] = [peptide['start'],peptide['end']]
+
+for gene in genes[1:]:
+  pos_dict[gene['qualifiers']['gene']] = [gene['start'],gene['end']]
+
+final_table = pd.DataFrame()
+
+for label in pos_dict:
+  pos = pos_dict[label]
+  new_row = table.loc[pos[0]:pos[1]].mean()
+  df = pd.DataFrame([new_row.rename(label)])
+  final_table = pd.concat([final_table,df], axis = 0)
+
+final_table.to_csv(f"{country}/correlation/annotation.csv")
