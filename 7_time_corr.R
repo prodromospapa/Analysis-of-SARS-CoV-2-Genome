2d21e8a0891aa5e0ee6bda18eef03b7b4cb3a83b
prodromospapa <prodromospapa@gmail.com>
2022-11-01 12:44:35 +0200

    thesis

--- 7_time_corr.R
+++ 7_time_corr.R
@@ -4,7 +4,7 @@ country <- toString(read.table("country.txt"))[1]
 file <- paste(country,"/correlation/correlation.csv",sep="")
 data <- read.table(file,sep=",", header=TRUE,row.names=1, quote = "", stringsAsFactors = FALSE)
 
-dates <- as.numeric(1:length(names(data)))
+dates <- as.numeric(1:length(names(data))) # nolint
 
 corre <- function(x){
 return(rcorr(dates,as.numeric(x),type="pearson")$r[2,1])}
