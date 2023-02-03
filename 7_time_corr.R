9f5ff842838959ecfc24231c8b5609bd910c812e
prodromospapa <prodromospapa@gmail.com>
2023-02-03 23:38:08 +0200

    thesis

--- 7_time_corr.R
+++ 7_time_corr.R
@@ -17,12 +17,17 @@ corr_values <- round(apply(data,1,corre),2)
 corr_p_values <- round(apply(data,1,p),2)
 
 merged_table <- do.call(rbind, Map(data.frame, 'Correlation'=corr_values, 'P-values'=corr_p_values))
-write.csv(merged_table,paste(country,'correlation/time_corr.csv',sep=""),row.names=TRUE)
+write.csv(merged_table,paste(country,'/correlation/time_corr.csv',sep=""),row.names=TRUE)
 
 system(paste("mkdir -p ",country,"/correlation/graphs",sep=""))
 for (orf in row.names(data)){
+    x <- dates
+    y <- as.numeric(data[orf,])
     png(file=paste(country,"/correlation/graphs/",orf,".png", sep = ""))
-    plot(dates, data[orf,],main = orf,xlab='dates',ylab='frequencies')
+    plot(x,y,main = orf,xlab='dates',ylab='frequencies',cex.main=2)
+    png(file=paste(country,"/correlation/graphs/",orf,"_lm.png", sep = ""))##
+    plot(x,y,main = orf,xlab='dates',ylab='frequencies',cex.main=2)##
+    abline(lm(y~x),col='red')##
     dev.off()
 }
 
